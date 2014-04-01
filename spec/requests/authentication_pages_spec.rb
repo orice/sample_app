e076a237739d24904a2de4cb0322770a2693e447
Olivia Rice <orice@wisc.edu>
2014-03-31 22:49:16 -0500

    exercise 2 complete

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -11,20 +11,22 @@ describe "Authentication" do
       before { click_button "Sign in" }
 
       it { should have_title('Sign in') }
-      it { should have_selector('div.alert.alert-error') }
+      it { should have_error_message('Invalid') }
+      #it { should have_selector('div.alert.alert-error') }
     end
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      before { valid_signin(user)}
+        #fill_in "Email",    with: user.email.upcase
+        #fill_in "Password", with: user.password
+        #click_button "Sign in"
+      #end
 
       it { should have_title(user.name) }
-      it { should have_link('Profile',     href: user_path(user)) }
-      it { should have_link('Sign out',    href: signout_path) }
+      it {should be_signed_in_as('User')}
+      #it { should have_link('Profile',     href: user_path(user)) }
+      #it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
     end
   end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,8 +6,30 @@ def valid_signin(user)
   click_button "Sign in"
 end
 
+def valid_signup(user)
+    fill_in "Name",         with: "Example User"
+    fill_in "Email",        with: "user@example.com"
+    fill_in "Password",     with: "foobar"
+    fill_in "Confirmation", with: "foobar"
+end
+
+
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
+end
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-success', text: message)
+  end
+end
+RSpec::Matchers.define :be_signed_in_as do |message|
+  match do |page|
+  	expect(page).to have_link ('Profile')
+  	expect(page).to have_link ('Sign out')
+    #page.should have_link ('Profile', href: user_path(user))
+    #page.should have_link ('Sign out', href: signout_path))
+	#page.should have_no_link('Sign In', href: signin_path)
   end
 end
