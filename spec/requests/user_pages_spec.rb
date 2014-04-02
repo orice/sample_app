3e6c5baed471341f4a74631081281e5462ade900
Olivia Rice <orice@wisc.edu>
2014-04-01 19:43:16 -0500

    hw 8

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -19,6 +19,7 @@ describe "User pages" do
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
+      
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
@@ -33,7 +34,7 @@ describe "User pages" do
     end
 
     describe "with valid information" do
-      let(:user) { FactoryGirl.build(:user) }
+      let(:user) { FactoryGirl.create(:user) }
       before {valid_signup(user)}
         #fill_in "Name",         with: "Example User"
         #fill_in "Email",        with: "user@example.com"
@@ -41,14 +42,13 @@ describe "User pages" do
         #fill_in "Confirmation", with: "foobar"
       #end
 
-      it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
-      end
-
       describe "after saving the user" do
+        
+        let(:user) { User.find_by(email: 'user@example.com') }
         before { click_button submit }
-        #let(:user) { User.find_by(email: 'user@example.com') }
-
+        it "should create a user" do
+          expect { click_button submit }.to change(User, :count).by(1)
+        end
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
         it { should have_success_message('Welcome') }
