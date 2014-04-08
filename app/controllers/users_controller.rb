d9c09488763ddf0975e2d327ae734d4f88770214
Olivia Rice <orice@wisc.edu>
2014-04-08 10:57:55 -0500

    ch 9 complete b4 exercises

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update, :index]
+  before_action :signed_in_user, only: [:edit, :update, :index, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
   
   def show
     @user = User.find(params[:id])
@@ -17,6 +18,11 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
 
   def update
     if @user.update_attributes(user_params)
@@ -45,11 +51,15 @@ class UsersController < ApplicationController
 	                                   :password_confirmation)
   end
       # Before filters
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 
   def signed_in_user
       store_location
       redirect_to signin_url, notice: "Please sign in." unless signed_in?
   end
+  
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
