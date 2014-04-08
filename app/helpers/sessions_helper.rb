628d59e1f65a95b87b3804e2a8ec9269c66d6ae9
Olivia Rice <orice@wisc.edu>
2014-04-08 09:31:47 -0500

    up to 9.3

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,5 @@
 module SessionsHelper
+
   def sign_in(user)
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
@@ -9,6 +10,11 @@ module SessionsHelper
   def current_user=(user)
     @current_user = user
   end
+
+  def current_user?(user)
+    user == current_user
+  end
+
 
   def current_user
     remember_token = User.hash(cookies[:remember_token])
@@ -25,4 +31,14 @@ module SessionsHelper
     cookies.delete(:remember_token)
     self.current_user = nil
   end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
+  end
+  
 end
