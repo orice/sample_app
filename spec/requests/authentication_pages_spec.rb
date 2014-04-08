fb7536a08569cabded565d1038c0e3895ba1319e
Olivia Rice <orice@wisc.edu>
2014-04-07 22:25:41 -0500

    up to 9.1.2

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -17,16 +17,16 @@ describe "Authentication" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before { valid_signin(user)}
+      before { sign_in user}
         #fill_in "Email",    with: user.email.upcase
         #fill_in "Password", with: user.password
         #click_button "Sign in"
       #end
 
       it { should have_title(user.name) }
-      it {should be_signed_in_as('User')}
-      #it { should have_link('Profile',     href: user_path(user)) }
-      #it { should have_link('Sign out',    href: signout_path) }
+      it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Sign out',    href: signout_path) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should_not have_link('Sign in', href: signin_path) }
     end
   end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,10 +1,17 @@
 include ApplicationHelper
 
-def valid_signin(user)
-  fill_in "Email",    with: user.email
-  fill_in "Password", with: user.password
-  click_button "Sign in"
-end
+def sign_in(user, options={})
+  if options[:no_capybara]
+    # Sign in when not using Capybara.
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.hash(remember_token))
+  else
+    visit signin_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
 
 def valid_signup(user)
     fill_in "Name",         with: "Example User"
@@ -24,12 +31,12 @@ RSpec::Matchers.define :have_success_message do |message|
     expect(page).to have_selector('div.alert.alert-success', text: message)
   end
 end
-RSpec::Matchers.define :be_signed_in_as do |message|
-  match do |page|
-  	expect(page).to have_link ('Profile')
-  	expect(page).to have_link ('Sign out')
+#RSpec::Matchers.define :be_signed_in_as do |message|
+ # match do |page|
+  #	expect(page).to have_link ('Profile')
+  #	expect(page).to have_link ('Sign out')
     #page.should have_link ('Profile', href: user_path(user))
     #page.should have_link ('Sign out', href: signout_path))
 	#page.should have_no_link('Sign In', href: signin_path)
-  end
+  #end
 end
